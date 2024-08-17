c47b5614ac04e18f92a69d5eba486ea9428ea264
Hussain <hsrar388@gmail.com>
2024-08-17 01:28:56 -0700

    Password-Generator

---
+++ password-Generator/app.js
@@ -0,0 +1,22 @@
+   
+    let inputArea = document.getElementById('input');   
+    let btn = document.getElementById('btn');
+    
+    let generatePassword = (j) =>{
+    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%'
+    let password = '';
+
+  for(let i=0; i < j; i++){
+    const randomIndex = Math.floor(Math.random() * characters.length);
+    password += characters[randomIndex];
+  }
+  return password
+    
+}
+btn.addEventListener('click', () => {
+    let passwordLength = 12;
+    newPassword = generatePassword(passwordLength);
+    inputArea.value = newPassword;  
+
+});
+// console.log(newPassword);
